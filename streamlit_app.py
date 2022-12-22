c2192f7e4e7484f8160a143dbeed31752e1446e1
Uzair Momin <121227872+Umomin223@users.noreply.github.com>
2022-12-22 15:42:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('🍨Omega 3 & Blueberry Oatmeal')
-streamlit.text('🍷Kale, Spinach & Rocket Smoothie')
-streamlit.text('🍳Hard-Boiled Free-Range Egg')
+streamlit.text('🍨 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍷 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍳 Hard-Boiled Free-Range Egg')
