114272b79261d35721a485b632815d68bc85d176
Uzair Momin <121227872+Umomin223@users.noreply.github.com>
2022-12-24 23:10:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🍨 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🍷 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍳 Hard-Boiled Free-Range Egg')
+
+import pandas as pd
+my_fruit_list_ds = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list_ds)
