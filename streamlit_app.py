3415bad83366b5b0f0bdac3f1d99e1d5753a111c
Uzair Momin <121227872+Umomin223@users.noreply.github.com>
2022-12-29 22:29:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,16 @@ streamlit.text('🍷 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍳 Hard-Boiled Free-Range Egg')
 
 import pandas as pd
-my_fruit_list_ds = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list_ds = my_fruit_list_ds.set_index('Fruit')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlist.dataframe(my_fruit_list)
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list_ds.index), ['Avacado','Strawberries'])
-#fruits_to_show = my_fruit_list_ds.loc[fruits_selected]
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avacado','Strawberries'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 #streamlit.dataframe(fruits_to_show)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response)
